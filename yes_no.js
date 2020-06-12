46e83638c783d84596778b67bb26b2add7fd157a
hungtb131 <66382816+hungtb131@users.noreply.github.com>
2020-06-12 09:24:55 +0700

    fix

--- yes_no.js
+++ yes_no.js
@@ -5,8 +5,6 @@ function checkyes() {
 function checkno() {
     let left = Math.round(Math.random()*100);
     let top = Math.round(Math.random()*500);
-    //A Hoà cho e hỏi xíu, khi kéo file.css vào HTML sau đó đến đoạn này (.style) thì phải có <style></style> trong html
-    // vậy trong 2 câu dưới đây thì (.style) thay bằng gì? để chạy đc chương trình
     document.getElementById("NoButton").style.left = left+"px";
     document.getElementById("NoButton").style.top = top+"px";
 }
